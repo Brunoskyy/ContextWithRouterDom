97065db90b789d8bf123dc312c5bc12538a50b47
Brunoskyy <arturbrunoferreira@gmail.com>
2021-10-04 13:05:03 -0300

    feature: Add useAuth hook in AuthContext

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,27 +1,49 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import api from "../services/api";
 
+type UserObject = {
+  email: string;
+  password: string;
+};
+
+type User = {
+  user: UserObject;
+};
 interface AuthContextData {
   signed: boolean;
+  user: object | null;
   Login(): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  async function Login() {
-    const response = await api.post("/login", {
-      email: "example@mail.com",
-      password: "123456",
-    });
+  const [user, setUser] = useState<object | null>(null);
 
+  async function Login() {
+    const response: User = {
+      user: {
+        email: "userteste",
+        password: "teste",
+      },
+    };
+
+    setUser(response.user);
+    // @ts-expect-error
+    api.defaults.headers.Authorization = `Bearer ${response.user.email}`;
     console.log(response);
   }
   return (
-    <AuthContext.Provider value={{ signed: true, Login }}>
+    <AuthContext.Provider value={{ signed: Boolean(user), user, Login }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
 
 export default AuthContext;
